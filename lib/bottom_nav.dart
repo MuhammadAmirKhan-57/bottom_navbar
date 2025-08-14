bb3a2becd5674521cc13db6f4c53632fcf635936
MuhammadAmirKhan-57 <m.aamirkhan5758@gmail.com>
2025-08-14 22:53:07 +0500

    Add HomeScreen

--- lib/bottom_nav.dart
+++ lib/bottom_nav.dart
@@ -1,8 +1,21 @@
+import 'package:bottom_navbar/home_screen.dart';
 import 'package:flutter/material.dart';
 
-class BottomNavScreen extends StatelessWidget {
+class BottomNavScreen extends StatefulWidget {
   const BottomNavScreen({super.key});
 
+  @override
+  State<BottomNavScreen> createState() => _BottomNavScreenState();
+}
+
+class _BottomNavScreenState extends State<BottomNavScreen> {
+  int selectedIndex = 0;
+
+  List<Widget> screenList = [
+    HomeScreen(),
+    // ProfileScreen(),
+    // AboutScreen(),
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -11,6 +24,7 @@ class BottomNavScreen extends StatelessWidget {
         title: Text('Bottom Navbar'),
         backgroundColor: Colors.indigo.shade200,
       ),
+      
     );
   }
 }
